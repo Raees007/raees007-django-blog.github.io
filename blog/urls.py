4854bae51315acf270c73ffe261005c5c05bc35e
Raees <r33857328@gmail.com>
2019-08-08 15:55:52 +0500

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+# from django.contrib.auth import views as auth_view
+
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft, name='post_draft'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/delete', views.post_delete, name='post_delete' ),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('signup/', views.signup, name='signup'),
+]
